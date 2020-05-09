9c2460711ef76b65401a739419b0a11eaad78333
Salathiel Genese <salathielgenese@gmail.com>
2020-05-10 01:12:07 +0400

    USerService#create to abstract API calls away

--- service/user.service.js
+++ service/user.service.js
@@ -9,7 +9,20 @@ export class UserService
     constructor({ API_PREFIX })
     {
         this.SEARCH_PATH = `${ API_PREFIX }/users`;
+        this.CREATE_PATH = `${ API_PREFIX }/users`;
 
+    }
+
+    async create({ name, email, roles })
+    {
+        return axios( this.CREATE_PATH, {
+            method: 'POST',
+            data: {
+                roles,
+                email,
+                name,
+            },
+        });
     }
 
     async search()
