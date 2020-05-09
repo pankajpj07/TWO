7aaa10c5214d754e9158b7f53255afc2caf4959f
Salathiel Genese <salathielgenese@gmail.com>
2020-05-09 21:15:18 +0400

    Move in-memory users to cache module

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,22 +1,13 @@
+import { USERS } from '../../service/cache';
+
+
+
 export default ( request, response ) =>
 {
     response.statusCode = 200;
     response.setHeader( 'Content-Type', 'application/json' );
     response.end( JSON.stringify({
         status: 'success',
-        content: [
-            {
-                id: 1,
-                name: 'Jain',
-                email: 'pankaj@jain.name',
-                roles: [ 'user:read', 'user:create', 'user:update', 'user:delete' ],
-            },
-            {
-                id: 2,
-                name: 'Salathiel',
-                email: 'salathiel@genese.name',
-                roles: [ 'product:read', 'product:create', 'product:update', 'product:delete', 'self:read', 'self:update' ],
-            },
-        ],
+        content: USERS,
     }) );
 };
---
+++ service/cache.js
@@ -0,0 +1,14 @@
+export const USERS = [
+    {
+        id: 1,
+        name: 'Jain',
+        email: 'pankaj@jain.name',
+        roles: [ 'user:read', 'user:create', 'user:update', 'user:delete' ],
+    },
+    {
+        id: 2,
+        name: 'Salathiel',
+        email: 'salathiel@genese.name',
+        roles: [ 'product:read', 'product:create', 'product:update', 'product:delete', 'self:read', 'self:update' ],
+    },
+];
