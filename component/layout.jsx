057b595a8cb9c62d08a7b96e6a91ae5630d06212
Salathiel Genese <salathielgenese@gmail.com>
2020-05-09 20:45:51 +0400

    Add scripts for bootstrap react

--- component/layout.jsx
+++ component/layout.jsx
@@ -26,9 +26,9 @@ export const WithLayout = ( Component ) =>
                 </Row>
                 Footer...
             </Container>
-            <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-            <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin></script>
-            <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+            <script src="https://unpkg.com/react/umd/react.production.min.js" crossOrigin="anonymous"></script>
+            <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossOrigin="anonymous"></script>
+            <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="anonymous"></script>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
         </>
         ;
